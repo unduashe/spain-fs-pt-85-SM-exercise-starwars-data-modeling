8acdda7df4c27312a23762592d836413c463d8d1
unduashe <unduashe@outlook.es>
2025-01-20 16:52:51 +0000

    ajustadas longitudes de string permitidas, cambio de tipo de algunos campos y añadidos email y teléfono

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -14,45 +14,44 @@ class User(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    password = Column(String(154), nullable=False)
+    password = Column(String(255), nullable=False)
+    email = Column(String(250))
+    phone_number = Column(Integer)
     favourites = relationship("favourites", back_populates="user")
     
 class Character(Base):
     __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250))
+    name = Column(String(250), nullable=False)
     gender = Column(String(250))
-    race = Column(String(250), nullable=False)
-    eye_colour = Column(String(250))
-    height = Column(String(250))
-    weight = Column(String(250))
+    race = Column(String(250))
+    eye_colour = Column(String(50))
+    height = Column(String(50))
+    weight = Column(String(50))
     master = Column(String(250))
     disciple = Column(String(250))
     image = Column(String(500))
-    # parent = relationship("Parent", back_populates="children")
 
 
 class Planet(Base):
     __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250))
+    name = Column(String(250), nullable=False)
     diameter = Column(Integer)
     weather = Column(String(100))
-    rotation_period = Column(Integer)
-    orbital_period = Column(Integer)
-    gravity = Column(Integer)
+    rotation_period = Column(Float)
+    orbital_period = Column(Float)
+    gravity = Column(Float)
     population = Column(Integer)
-    terrain = Column(String(250))
+    terrain = Column(String(100))
     image = Column(String(500))
 
 class Favourites(Base):
     __tablename__ = 'Favourites'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     favourite_id = Column(Integer, primary_key=True)
     character_id = Column(Integer, ForeignKey("character.id"))
     planet_id = Column(Integer, ForeignKey("planet.id"))
-    user_id = Column(Integer, ForeignKey("User.id"))
+    user_id = Column(Integer, ForeignKey("User.id"), nullable=False)
     user = relationship("User", back_populates="favourites")
 
 
